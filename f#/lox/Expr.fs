e21742a1f955bb5c4ca0b87fd88d0a4542413c4e
Jun <lust4life.jun@gmail.com>
2023-11-16 09:20:38 +0800

    representing-code

---
+++ f#/lox/Expr.fs
@@ -0,0 +1,7 @@
+namespace lox
+
+type Expr =
+    | Binary of left: Expr * operator: Token * right: Expr
+    | Grouping of expression: Expr
+    | Literal of value: obj
+    | Unary of operator: Token * right: Expr
